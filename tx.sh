73f3430224d2dfd74249c99fe4b72e903bafce60
Danil <danil90381@mail.ru>
2021-03-19 15:38:27 +0200

    Add files via upload

---
+++ tx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git;
+sudo apt install -y python-pip;
+sudo apt install -y python3-m2crypto;
+sudo apt install -y python-m2crypto;
+sudo apt-get install python-m2crypto -y;
+sudo apt-get install openssl -y;
+sudo apt-get install libnl-3-dev -y;
+sudo apt-get install libnl-3-200 -y;
+sudo apt-get install libgcrypt20-dev -y;
+sudo pip install setuptools;
+sudo pip install future;
+sudo pip install M2Crypto;
